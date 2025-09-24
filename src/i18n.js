87c867ccfc7ad472a9a5ff127142abd07b360735
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-09-23 20:05:06 -0500

    i18n & entities added

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,24 @@
 /**
  * i18n.js
- * Main internaltionalization configuration file,
+ * Main internationalization configuration file,
  * File for translate texts according to the selected language
  */
+import en from './locales/en.json';
+import es from './locales/es.json';
+import {createI18n} from "vue-i18n";
+
+/**
+ * i18n instance for internationalization
+ * legacy: false to use Composition API
+ * locale: default language
+ * fallbackLocale: fallback language if the selected one is not available
+ * messages: object with translations for each language
+ */
+const i18n = createI18n( {
+    legacy: false,
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: { en, es }
+});
+
+export default i18n;
